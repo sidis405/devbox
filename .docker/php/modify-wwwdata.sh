a5c1d1ddb6a5083d0e89142e6bb115f3495c7902
ivano <ivanomatteo@gmail.com>
2019-04-03 18:34:54 +0200

    fix previous commit

--- .docker/php/modify-wwwdata.sh
+++ .docker/php/modify-wwwdata.sh
@@ -7,8 +7,8 @@ if [[ -n "$CURR_USER_ID" ]] && [[ -n "$CURR_GROUP_ID" ]]; then
     #groupadd -g $CURR_GROUP_ID defuser && echo "gruppo creato con successo" || exit 1
     #useradd -mu $CURR_USER_ID -g $CURR_GROUP_ID -s /bin/bash defuser && echo "utente creato con successo" || exit 1
 
-    usermod -u $CURR_GROUP_ID www-data && echo "gruppo www-data modificato con successo" || exit 1
-    groupmod -g $CURR_USER_ID www-data && echo "utente www-data modificato con successo" || exit 1
+    usermod -u $CURR_USER_ID www-data && echo "gruppo www-data modificato con successo" || exit 1
+    groupmod -g $CURR_GROUP_ID www-data && echo "utente www-data modificato con successo" || exit 1
 
     passwd root << EOF
 Docker!
